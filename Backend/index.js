efcb858ebbb6dbf28e171d50592d54e08febb990
Tridib11 <tridibghosh12345@gmail.com>
2024-02-24 05:20:35 +0530

    Files Modified

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,16 @@ app.post("/todo", function (req, res) {
   //put it in mongodb
 });
 app.get("/todos", function (req, res) {});
-app.put("/completed", function (req, res) {});
+app.put("/completed", function (req, res) {
+  const updatePayload = req.body;
+  const parsedPayload = updateTodo.safeParse(updatePayload);
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: "You sent wrong inputs",
+    });
+    return;
+  }
+});
 
 PORT = 3000;
 app.listen(PORT, () => {
