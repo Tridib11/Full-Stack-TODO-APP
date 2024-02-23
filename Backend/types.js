8052d5f5f52b8af0bc32c2f47ae64eb98b844e8d
Tridib11 <tridibghosh12345@gmail.com>
2024-02-24 04:56:07 +0530

    Files Modified

---
+++ Backend/types.js
@@ -0,0 +1,22 @@
+//Schema
+/*
+    {
+        title : String,
+        description : String
+    }
+    {
+        id:String
+    }
+*/
+const zod = require("zod");
+const createTodo = zod.object({
+  title: zod.string(),
+  description: zod.string(),
+});
+const updateTodo = zod.object({
+  id: zod.string(),
+});
+module.exports = {
+  createTodo: createTodo,
+  updateTodo: updateTodo,
+};
