1e81c802bc6721c99bda9dda45f4bbbed3fcd120
Tridib11 <tridibghosh12345@gmail.com>
2024-02-24 05:55:58 +0530

    Backend Files added

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const { createTodo, updateTodo } = require("./types");
+const { todo } = require("./db");
 app.use(express.json());
 
-app.post("/todo", function (req, res) {
+app.post("/todo", async function (req, res) {
   const createPayload = req.body;
   const parsedPayload = createTodo.safeParse(createPayload);
   if (!parsedPayload.success) {
@@ -13,9 +14,24 @@ app.post("/todo", function (req, res) {
     return;
   }
   //put it in mongodb
+  await todo.create({
+    title: createPayload.title,
+    description: createPayload.description,
+    completed: false,
+  });
+  res.json({
+    msg: "Todo Created",
+  });
 });
-app.get("/todos", function (req, res) {});
-app.put("/completed", function (req, res) {
+
+app.get("/todos", async function (req, res) {
+  const todos = await todo.find({});
+  res.json({
+    todos,
+  });
+});
+
+app.put("/completed", async function (req, res) {
   const updatePayload = req.body;
   const parsedPayload = updateTodo.safeParse(updatePayload);
   if (!parsedPayload.success) {
@@ -24,6 +40,15 @@ app.put("/completed", function (req, res) {
     });
     return;
   }
+  await todo.update(
+    {
+      _id: req.body.id,
+    },
+    { completed: true }
+  );
+  res.json({
+    msg: "Todo marked as completed",
+  });
 });
 
 PORT = 3000;
