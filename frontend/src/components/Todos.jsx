663291da65bd0f2a7562dc6c0c0cb55b1a9e15a6
Tridib11 <tridibghosh12345@gmail.com>
2024-02-24 17:26:34 +0530

    All files added

--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,7 +1,20 @@
-export function Todos(){
+/*
+todos=[
+    {
+        title:"Go to the gym",
+        description:"Go to the gym"
+    }
+]
+*/
+export function Todos({todos}){
+
     return <div>
-        <h1>Go to gym</h1>
-        <h2>You need to go to gym</h2>
-        <button>Mark as completed</button>
+        {todos.map(function(todo){
+            return <div>
+                <h1>{todo.title}</h1>
+                <h2>{todo.description}</h2>
+                <button>{todo.completed==true ? "Completed":"Mark as complete"}</button>
+            </div>
+        })}
     </div>
 }
